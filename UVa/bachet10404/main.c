225e968cc7a5cec0cdbd505dce127e2bdd91db4f
Thomas Laumann <laumann.thomas@gmail.com>
2011-05-23 22:53:51 +0200

    Bachet's game accepted at UVa (rewritten for more pointer usage) - Run time: 0.520 seconds

--- UVa/bachet10404/main.c
+++ UVa/bachet10404/main.c
@@ -7,35 +7,34 @@
 int
 main(int argc, char **argv)
 {
-	long 	n;	/* number of stones */
-	int 	m;	/* number of following arguments */
-	int 	i,j,D;	/* iteration variables */
+	long 	n,	/* number of stones */
+		i;
+	int 	m,	/* number of following arguments */
+		_; 	/* ignored */
 
-	int 	*opts,
-		*game;
+	int	*ms,	/* the m's (set of numbers allowable per game to remove) */
+		*game,	/* the actual game */
+		*k;	/* pointer to iterate through ms */
 
 	while (fscanf(stdin, "%ld %d", &n, &m) != EOF) { 	/* Init */
-		game = (int*)calloc(n, sizeof(int));
-		opts = (int*)calloc(m, sizeof(int));
+		game 	= (int*)calloc(n+1, sizeof(int));
+		ms 	= (int*)calloc(m+1, sizeof(int));
 
-		for (i=0; i<m; i++) {
-			D = fscanf(stdin, "%d", &opts[i]);
-			game[opts[i]-1] = 1;
-		}
+		for (i=0; i<m; i++)
+			_ = scanf("%d", ms+i);
 
-		for (i=0; i<n; i++)				/* Process */
-			for (j=0; j<m; j++) {
-				if (game[i] || i-opts[j] < 0)	/* Already true, go on */
-					continue;
-				if (!game[i-opts[j]]) {		/* test */
-					game[i] = 1;
+		*(game+1) = 1;
+		*(ms+m) = 0;
+		for (i=2; i<=n; i++)				/* Process */
+			for (k = ms; *k ; k++)
+				if (!(i-*k < 0) && !*(game+i-*k) && !*(game+i)) {
+					*(game+i) = 1;
 					break;
 				}
-			}
 		
-		printf("%s\n", game[n-1] ? "Stan wins" : "Ollie wins");
+		printf("%s wins\n", *(game+n) ? "Stan" : "Ollie");
 		free(game);
-		free(opts);
+		free(ms);
 	}
 	exit(0);
 }
